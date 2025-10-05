817c74b3f7dc0a00eddae2c1b40ce8f1ea179278
Артём <eliseytsev.artem@mail.ru>
2025-10-05 21:34:14 +0300

    1.3

---
+++ 1.3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(void) {
+	int powerCarInHorse;
+	float powerCarInKWT;
+
+	std::cout << "Enter the power of the car in horsepower: ";
+	std::cin >> powerCarInHorse;
+
+	powerCarInKWT = powerCarInHorse * 0.7457;
+
+	std::cout << "Power in horsepower: " << powerCarInHorse << " power in kWt: " << powerCarInKWT;
+}
